4c496455f477ad47300d5885da4a650597a45fe9
sulthanrizal <surie0312@gmail.com>
2024-03-10 15:37:00 +0700

    create loop background color page

---
+++ src/@page/mapping/backgroundImg.js
@@ -0,0 +1,38 @@
+
+export const products = [
+    {
+        image1: "https://vimcosmo.com/pics/fenty-2.webp",
+        image2: "https://vimcosmo.com/pics/fenty-1.webp",
+        price: "23.95$",
+        name: 'Fenty'
+    },
+    {
+        image1: "https://vimcosmo.com/pics/ilia-1.webp",
+        image2: "https://vimcosmo.com/pics/ilia-2.webp",
+        price: "38.65$",
+        name: 'Illia'
+    },
+    {
+        image1: 'https://vimcosmo.com/pics/kenzoki-2.webp',
+        image2: 'https://vimcosmo.com/pics/kenzoki-1.webp',
+        price: '43.95$',
+        name: 'Kenzoki hydration'
+    },
+    {
+        image1: 'https://vimcosmo.com/pics/pixi-1.webp',
+        image2: 'https://vimcosmo.com/pics/pixi-2.webp',
+        price: '36.95$',
+        name: 'Pixi'
+
+    }
+
+]
+
+export const backgroundImg = [
+    {
+        backgroundImgHome: 'https://vimcosmo.com/pics/home-cover.webp'
+    },
+    {
+        backgroundImgShop: 'https://vimcosmo.com/pics/shop-hero.webp'
+    },
+]
